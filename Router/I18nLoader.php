938c20edc90a4f67c1fea3d4266fc04762f8169e
Tim Timmermans <tim@servexs.nl>
2019-03-04 08:34:02 +0100

    do not force a locale on routes

--- Router/I18nLoader.php
+++ Router/I18nLoader.php
@@ -63,14 +63,14 @@ class I18nLoader
                 if (count($locales) > 1) {
                     $catchMultipleRoute = clone $route;
                     $catchMultipleRoute->setPath($pattern);
-                    $catchMultipleRoute->setDefault('_locales', $locales);
+                    //$catchMultipleRoute->setDefault('_locales', $locales);
                     $i18nCollection->add(implode('_', $locales).I18nLoader::ROUTING_PREFIX.$name, $catchMultipleRoute);
                 }
 
                 foreach ($locales as $locale) {
                     $localeRoute = clone $route;
                     $localeRoute->setPath($pattern);
-                    $localeRoute->setDefault('_locale', $locale);
+                    //$localeRoute->setDefault('_locale', $locale);
                     $i18nCollection->add($locale.I18nLoader::ROUTING_PREFIX.$name, $localeRoute);
                 }
             }
